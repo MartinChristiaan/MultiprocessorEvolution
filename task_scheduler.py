1dbc3c2271e29761dfc04b6677194fd95f29e53c
MartinChristiaan <martinvanleeuwen95@gmail.com>
2019-03-16 17:53:26 +0100

    More tasks

--- task_scheduler.py
+++ task_scheduler.py
@@ -3,13 +3,31 @@ import numpy as np
 
 
 
+
+
 class ARMv8():
     def __init__(self,V):
         cycles_per_task_s1 = [192450,276390,477300,0,1125420,950540,692800,782590,442410,305310,60300]
         cycles_per_task_s2 = [192450,272500,561600,970500,911500,950540,511540,660810,391580,306990,60300]
-        k_f = 1
+        k_f = 192450/(295*10**-9)
         self.time_per_task_s1 = np.array(cycles_per_task_s1)/(k_f * V)
-    
+
+class Adreno():
+    def __init__(self,V):
+        cycles_per_task_s1 = [113060 ,165660 ,284000 ,0,685320 ,656110 ,398220 ,472050 ,274370 ,203750,39050]
+        cycles_per_task_s2 = [113060 ,160630 ,329560 ,593120 ,542880 ,553000 ,300600 ,450600 ,237750 ,209750 ,39050 ]
+        k_f = 165660/(545*10**-9)
+        self.time_per_task_s1 = np.array(cycles_per_task_s1)/(k_f * V)
+
+class MIPS():
+    def __init__(self,V):
+        cycles_per_task_s1 = [98540  ,124380  ,217450  ,0,545310  ,456790  ,325670  ,369120  ,208450  ,136170 ,28640 ]
+        cycles_per_task_s2 = [98540  ,117900  ,267450  ,482050  ,434320  ,423400  ,236540  ,302370  ,195780  ,138320  ,28640]
+        k_f = 165660/(545*10**-9)
+        self.time_per_task_s1 = np.array(cycles_per_task_s1)/(k_f * V)
+
+
+
 # Load some node array
 nodes = [ARMv8(1),ARMv8(1),ARMv8(1),ARMv8(1),ARMv8(1),ARMv8(1)]
 
