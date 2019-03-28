4d723ef790c55bb81f8ae62f59221aa5adbba197
Leeuwen, M.C. van <martinvanleeuwen95@hotmail.com>
2019-03-28 14:15:20 +0100

    fine tune analysis

--- generation_analysis.py
+++ generation_analysis.py
@@ -2,6 +2,26 @@ import pandas as pd
 import numpy as np
 from selection import pareto_rank,tournament,crowding_distance
 
+def insert_source_config(generation_df):
+    source_df = pd.read_csv('generations/source.csv') 
+    result_df = pd.DataFrame()
+    for i,row in generation_df.iterrows():
+        mydata = list(row)
+        platform_id = mydata[15]
+        platform = source_df.iloc[platform_id]
+        proc_type_distribution = platform['NodeProcessorTypeDistribution']
+        node_pref = platform['Number of Processors']
+        combi_task = platform['Combined Tasks']
+        mydata[15] = proc_type_distribution
+        row_df = pd.DataFrame([mydata],columns = source_df.columns[:-5])
+        result_df = result_df.append(row_df)
+    return result_df
+
+
+
+
+
+
 
 def add_paretorank_and_save(generation_df,gen):
     generation_df = generation_df[generation_df['Latency']<9000]
@@ -58,7 +78,7 @@ def add_paretorank_and_save(generation_df,gen):
         combi_task = row['Combined Tasks']
         
         if combi_task[1] == combi_task[4]:
-            combi_tasks.append("No Transfrom")
+            combi_tasks.append("(1, 1)")
         else:
             combi_tasks.append(combi_task)
 
@@ -67,8 +87,10 @@ def add_paretorank_and_save(generation_df,gen):
     generation_df["no_Adreno"] = no_adreno
     generation_df["no_ARMv8"] = no_arm
     generation_df['Combined Tasks'] = combi_tasks
-    print(proc_used)
-    print(full_volt)
-    generation_df.to_csv('generations/ComboNodesAndProcTypes_ranked.csv',index=False)
+    
+    generation_df.to_csv(gen,index=False)
 
-add_paretorank_and_save(pd.read_csv('generations/ComboNodesAndProcTypes.csv'),0)
+#result = insert_source_config(pd.read_csv("generations/generation21.csv"))
+#add_paretorank_and_save(result,"generations/evolution_result.csv")
+#dd_paretorank_and_save(pd.read_csv('generations/ComboNodesAndProcTypes.csv'),0)
+add_paretorank_and_save(pd.read_csv('generations/Evolution_vs_Source.csv'),'generations/Evolution_vs_source_reranked.csv')
